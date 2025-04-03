51e9e80520a5c21f899fcfc56f98a388b6e0b76d
RomanB6262 <R.Babuls6262@student.leedsbeckett.ac.uk>
2025-04-03 03:35:55 +0100

    Delete Phishguard Ext directory

--- Phishguard Ext/popup.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("supportButton").addEventListener("click", function () {
-  // Redirect to the support.html page
-  window.location.href = "support.html";
-});
-
-document.getElementById("settingsButton").addEventListener("click", function () {
-  // Redirect to the settings.html page
-  window.location.href = "settings.html";
-});
